bd7acee9cf4492391a8ad2c11d81b2e5fc7f84a5
CenturHuila <proyectocenturhuila@gmail.com>
2021-10-28 10:35:13 -0500

    filtros finales

--- src/app/views/tourist-providers/tourist-providers.component.ts
+++ src/app/views/tourist-providers/tourist-providers.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { strictEqual } from 'assert';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { PageChangedEvent } from 'ngx-bootstrap/pagination';
+import { runInThisContext } from 'vm';
+import { ServicesService } from '../../services/services/services.service';
 import { TouristProvidersService } from '../../services/tourist-providers/tourist-providers.service';
+import { TownshipsService } from '../../services/townships/townships.service';
 import { UsersService } from '../../services/users/users.service';
 
 @Component({
@@ -10,37 +14,109 @@ import { UsersService } from '../../services/users/users.service';
   templateUrl: 'tourist-providers.component.html',
   styleUrls: ['./tourist-providers.component.css']
 })
-export class TouristProvidersComponent implements OnInit{
+export class TouristProvidersComponent implements OnInit {
 
   modalRef?: BsModalRef;
   touristProvidersData = [];
+  touristAllProvidersData = [];
+  townshipsData = [];
   returnedArray?: string[];
+  returnedArrayTownship?: string[];
   documentToEdit = {};
   documentToDelete: string;
+  servicesData: any[];
+  returnedArrayServices: any[];
+  results: boolean = true;
+  valueToSearch: string = "";
+
   constructor(private modalService: BsModalService,
     private touristProvidersService: TouristProvidersService,
     private userService: UsersService,
-    private router: Router,) { }
+    private router: Router,
+    private townshipsServices: TownshipsService,
+    private servicesService: ServicesService) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.loadData();
-    this.userService.getCurrentUser().then((res =>{
+    this.userService.getCurrentUser().then((res => {
       if (res) {
-        console.log(res)
-      }else{
+      } else {
         this.router.navigate(['/login'])
       }
     }));
   }
-  loadData(){
+
+  loadData() {
+    this.results=true
+    
     this.touristProvidersService.get().subscribe(response => {
       this.touristProvidersData = [];
+      this.touristAllProvidersData = [];
       response.forEach(element => {
         this.touristProvidersData.push(element.payload.doc.data());
-      }); 
+      });
+      this.touristAllProvidersData = this.touristProvidersData;
       this.returnedArray = this.touristProvidersData.slice(0, 10);
+
     })
+    this.townshipsServices.get().subscribe(response => {
+      this.townshipsData = [];
+      response.forEach(element => {
+        this.townshipsData.push(element.payload.doc.data());
+      });
+      this.returnedArrayTownship = this.townshipsData.slice(0, 100);
+    })
+    this.servicesService.get().subscribe(response => {
+      this.servicesData = [];
+      response.forEach(element => {
+        this.servicesData.push(element.payload.doc.data());
+      });
+      this.returnedArrayServices = this.servicesData.slice(0, 100);
+    });
+
   }
+  loadDataFilterByCity(city: string) {
+    //filtrado ciudad
+    this.results=true
+    this.returnedArray = [];
+    this.returnedArray = this.touristAllProvidersData.filter((item: any) => {
+      if(item.township === city){
+        this.results=true
+        return item;
+      }
+    });
+    this.touristProvidersData = this.returnedArray;
+  }
+
+  loadDataFilterByServices(service: string) {
+    //filtrado Services
+    this.returnedArray = [];
+    this.returnedArray = this.touristAllProvidersData.filter((item: any) => {
+      if(item.services.includes(service)){
+        this.results=true
+        return item;
+      }
+    });
+    this.touristProvidersData = this.returnedArray;
+  }
+  
+  loadDaraFilterByName(name: string){
+    this.returnedArray = [];
+    this.returnedArray = this.touristAllProvidersData.filter((item: any) => {
+      if(item.name.toUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(name.toUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""))){
+        this.results=true
+        return item;
+      }else if (name== null || name.length <=2) {
+        this.results=true
+        this.returnedArray = [];
+      this.returnedArray = this.touristAllProvidersData;
+      } else{
+        this.results=false
+      }  
+    });
+    this.touristProvidersData = this.returnedArray;
+  }
+
   openModal(template: TemplateRef<any>, documentToEdit?) {
     this.documentToEdit = documentToEdit;
     this.modalRef = this.modalService.show(template);
@@ -51,7 +127,7 @@ export class TouristProvidersComponent implements OnInit{
     this.modalRef = this.modalService.show(template);
     this.modalRef.setClass('modal-lg');
   }
-  closeModal(){
+  closeModal() {
     this.modalRef.hide();
     this.loadData();
   }
--- src/app/views/townships/townships.component.ts
+++ src/app/views/townships/townships.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { log } from 'console';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { PageChangedEvent } from 'ngx-bootstrap/pagination';
 import { TownshipsService } from '../../services/townships/townships.service';
 import { UsersService } from '../../services/users/users.service';
+import { ZonesService } from '../../services/zones/zones.service';
 
 @Component({
   selector: 'app-townships',
@@ -14,32 +16,77 @@ export class TownshipsComponent implements OnInit{
 
   modalRef?: BsModalRef;
   touristProvidersData = [];
+  touristAllProvidersData = [];
   returnedArray?: string[];
   documentToEdit = {};
+  results: boolean = true;
   documentToDelete: string;
+  zoneData: any[];
+  returnedArrayZones: any[];
   constructor(private modalService: BsModalService,
     private townshipsService: TownshipsService,
     private userService: UsersService,
-    private router: Router,) { }
+    private router: Router,
+    private zonesService: ZonesService) { }
 
   ngOnInit(){
     this.loadData();
     this.userService.getCurrentUser().then((res =>{
       if (res) {
-        console.log(res)
+        
       }else{
         this.router.navigate(['/login'])
       }
     }));
   }
   loadData(){
+    this.results=true
     this.townshipsService.get().subscribe(response => {
       this.touristProvidersData = [];
       response.forEach(element => {
         this.touristProvidersData.push(element.payload.doc.data());
       }); 
+      this.touristAllProvidersData = this.touristProvidersData;
       this.returnedArray = this.touristProvidersData.slice(0, 10);
     })
+
+    this.zonesService.get().subscribe(response => {
+      this.zoneData = [];
+      response.forEach(element => {
+        this.zoneData.push(element.payload.doc.data());
+      }); 
+      this.returnedArrayZones = this.zoneData.slice(0, 10);
+      
+    })
+  }
+
+  loadDataFilterByZones(zone: string) {
+    //filtrado Services
+    this.returnedArray = [];
+    this.returnedArray = this.touristAllProvidersData.filter((item: any) => {
+      if(item.zone === (zone)){
+        return item;
+        this.results=true
+      }
+    });
+    this.touristProvidersData = this.returnedArray;
+  }
+
+  loadDaraFilterByName(name: string){
+    this.returnedArray = [];
+    this.returnedArray = this.touristAllProvidersData.filter((item: any) => {
+      if(item.name.toUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(name.toUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""))){
+        this.results=true
+        return item;
+      }else if (name== null || name.length <=2) {
+        this.results=true
+        this.returnedArray = [];
+      this.returnedArray = this.touristAllProvidersData;
+      } else{
+        this.results=false
+      }  
+    });
+    this.touristProvidersData = this.returnedArray;
   }
   openModal(template: TemplateRef<any>, documentToEdit?) {
     this.documentToEdit = documentToEdit;
